d11c766c2e2edc491bf075b25fad066cf983fc00
Eric H <erich@stradigi.ai>
2020-11-10 09:39:13 -0500

    correcting paste error

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -5,7 +5,6 @@ mdkir -p ~/.vim/pack/plugins/start
 
 ## clone plugins
 
-git clone --depth=1 git@github.com:vim-airline/vim-airline.git
-/.vim/pack/plugins/start/airline
+git clone --depth=1 git@github.com:vim-airline/vim-airline.git ~/.vim/pack/plugins/start/airline
 
 git clone --depth=1 git@github.com:vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/airline-themes
