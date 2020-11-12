cda30286f9fe678aa5fd99d9d30a20e38927a653
blizter <blizter47@gmail.com>
2020-11-12 18:53:04 -0500

    fixing vim plugins install script

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -1,7 +1,8 @@
 #! /bin/bash 
 
 ## creating vim plugins folders
-mdkir -p ~/.vim/pack/plugins/start
+rm -rf ~/.vim/pack/
+mkdir -p ~/.vim/pack/plugins/start
 
 ## clone plugins
 git clone --depth=1 git@github.com:vim-airline/vim-airline.git ~/.vim/pack/plugins/start/airline
