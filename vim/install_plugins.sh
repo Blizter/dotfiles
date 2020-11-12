643bda5643e9834c2237a765cb65d3c6bbca9762
Blizter <blizter47@gmail.com>
2020-11-12 16:33:07 -0500

    cleaning files

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -4,7 +4,5 @@
 mdkir -p ~/.vim/pack/plugins/start
 
 ## clone plugins
-
 git clone --depth=1 git@github.com:vim-airline/vim-airline.git ~/.vim/pack/plugins/start/airline
-
 git clone --depth=1 git@github.com:vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/airline-themes
