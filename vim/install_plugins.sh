460ba30f77c427c55fb3024991ad62f7244824af
Blizter <blizter47@gmail.com>
2020-11-14 21:07:56 -0500

    adding fugitive and gitgutter

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -9,4 +9,7 @@ git clone --depth=1 git@github.com:vim-airline/vim-airline.git ~/.vim/pack/plugi
 git clone --depth=1 git@github.com:vim-airline/vim-airline-themes.git ~/.vim/pack/plugins/start/airline-themes
 git clone --depth=1 git@github.com:ajmwagar/vim-deus.git ~/.vim/pack/plugins/start/vim-deus
 git clone --depth=1 git@github.com:sheerun/vim-polyglot.git ~/.vim/pack/plugins/start/vim-polyglot
-
+git clone --depth=1 git@github.com:airblade/vim-gitgutter.git ~/.vim/pack/plugins/start/vim-gitgutter \
+    && vim -u NONE -c "helpztags ~/.vim/pack/plugins/start/vim-gitgutter" -c q
+git clone --depth=1 git@github.com:tpope/vim-fugitive.git ~/.vim/pack/plugins/start/vim-fugitive \
+    && vim -u NONE -c "helpztags ~/.vim/pack/plugins/start/vim-fugitive" -c q
