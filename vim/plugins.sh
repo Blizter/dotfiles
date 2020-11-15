6858044c3a75e885ddf92030000e12f4eb7481d3
blizter <blizter47@gmail.com>
2020-11-15 16:49:46 -0500

    add a new plugins

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -6,7 +6,8 @@ git clone --depth=1 git@github.com:vim-airline/vim-airline-themes.git ~/.vim/pac
 git clone --depth=1 git@github.com:ajmwagar/vim-deus.git ~/.vim/pack/plugins/start/vim-deus
 git clone --depth=1 git@github.com:sheerun/vim-polyglot.git ~/.vim/pack/plugins/start/vim-polyglot
 git clone --depth=1 git@github.com:airblade/vim-gitgutter.git ~/.vim/pack/plugins/start/vim-gitgutter
-git clone --depth=1 git@github.com:tpope/vim-fugitive.git ~/.vim/pack/plugins/start/vim-fugitive 
+git clone --depth=1 git@github.com:tpope/vim-fugitive.git ~/.vim/pack/plugins/start/vim-fugitive
+git clone --depth=1 git@github.com:tpope/vim-vinegar.git ~/.vim/pack/plugins/start/vim-vinegar
 
 ## Helptags generation for the different plugins
 vim -u NONE -c "helpztags ~/.vim/pack/plugins/start/vim-fugitive" -c q
