ab1caaec9ca0a3905a48d67d3e000a4471f12b21
blizter <blizter47@gmail.com>
2020-11-15 16:18:04 -0500

    removing one file and adding spotify to install

--- install.sh
+++ install.sh
@@ -4,11 +4,24 @@
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade
 
 ## install the different tools
-sudo apt install curl wget git vim tmux autojump parallel fonts-powerline
+sudo apt install curl wget git vim tmux autojump parallel gnome-tweaks fonts-powerline
 
 ## install python tools
 sudo apt install python3-dev python3-pip
 pip3 install bpytop
 
 # installing vim plugins
-source ./vim/install_plugins.sh
+## creating vim plugins folders
+rm -rf ~/.vim/pack/
+mkdir -p ~/.vim/{colors,pack/plugins/start}
+
+## prallilize plugins repo cloning
+parallel -a vim/plugins.sh
+
+## installing other software or packges
+
+#Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
--- vim/install_plugins.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash 
-
-## creating vim plugins folders
-rm -rf ~/.vim/pack/
-mkdir -p ~/.vim/{colors,pack/plugins/start}
-
-## prallilize plugins repo cloning
-parallel -a plugins.sh
