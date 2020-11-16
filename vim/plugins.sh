9a89e0031fba5d7465d8ba1599c26dcd23bf9c12
blizter <blizter47@gmail.com>
2020-11-15 20:52:47 -0500

    adding markdown and md preview

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -9,8 +9,13 @@ git clone --depth=1 git@github.com:airblade/vim-gitgutter.git ~/.vim/pack/plugin
 git clone --depth=1 git@github.com:tpope/vim-fugitive.git ~/.vim/pack/plugins/start/vim-fugitive
 git clone --depth=1 git@github.com:tpope/vim-vinegar.git ~/.vim/pack/plugins/start/vim-vinegar
 git clone --depth=1 git@github.com:preservim/tagbar.git ~/.vim/pack/plugins/start/tagbar
+git clone --depth=1 git@github.com:plasticboy/vim-markdown.git ~/.vim/pack/plugins/start/vim-markdown
+git clone --depth=1 git@github.com:iamcco/markdown-preview.nvim.git ~/.vim/pack/plugins/start/markdown-preview
+
 
 ## Helptags generation for the different plugins
 vim -u NONE -c "helpztags ~/.vim/pack/plugins/start/vim-fugitive" -c q
 vim -u NONE -c "helpztags ~/.vim/pack/plugins/start/vim-gitgutter" -c q
 
+# installing markdown preview
+cd ~/.vim/pack/plugins/start/markdown-preview && yarn install && cd ~
