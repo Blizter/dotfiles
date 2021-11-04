d713105f9760a49407403c906a13ca996507ae85
Blizter <blizter47@gmail.com>
2021-11-04 19:28:27 -0400

    fix zsh completion url

--- install.sh
+++ install.sh
@@ -96,9 +96,9 @@ sudo wget https://github.com/hadolint/hadolint/releases/download/v2.7.0/hadolint
 [ ! -d "${HOME}/.oh-my-zsh/completion" ] && \
     mkdir -p ${HOME}/.oh-my-zsh/completions; \
     chmod -R 755 ${HOME}/.oh-my-zsh/completions; \
-    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubectx.zsh \
+    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubectx.zsh \
         -O ${HOME}/.oh-my-zsh/completions/_kubectx.zsh; \
-    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/kubens.zsh \
+    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubens.zsh \
         -O ${HOME}/.oh-my-zsh/completions/_kubens.zsh; \
 
 # AWS cli version 2 Install
