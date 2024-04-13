d77823a3772ad3366f7759d6aeede9f81680cce0
Blizter <Blizert47@gmail.com>
2024-04-13 15:50:03 -0400

    improve install scrip

--- dotfiles/.zprofile
+++ dotfiles/.zprofile
@@ -4,34 +4,32 @@
 export PATH="/usr/bin:/usr/local/bin:/bin:${PATH}"
 
 # set PATH so it includes user's private bin if it exists
-[ -d "${HOME}/bin" ] && PATH="${HOME}/bin:${PATH}"
-
-# set PATH so it includes user's private bin if it exists
 [ -d "${HOME}/.local/bin" ] && PATH="${HOME}/.local/bin:${PATH}"
 
 # go binary variables
-[ -d "/usr/local/go/bin" ] && GOPATH="${HOME}/go"; \
-                              GOBIN="${GOPATH}/bin"; \
-                              PATH="/usr/local/go/bin:${GOPATH}:${GOBIN}:${PATH}"; \
+[ -d "${HOME}/.local/go/bin" ] && GOPATH="${HOME}/.local/go/"; \
+                                GOBIN="${GOPATH}/bin"; \
+                                PATH="${GOPATH}:${GOBIN}:${PATH}"; \
 
 # pyenv python binaries
-[ -d "${HOME}/.pyenv/bin" ] &&  PYENV_ROOT="${HOME}/.pyenv"; \
+[ -d "${HOME}/.local/pyenv/bin" ] &&  PYENV_ROOT="${HOME}/.local/pyenv/"; \
                                 PATH="${PYENV_ROOT}/bin:${PATH}"; \
                                 eval "$(pyenv init --path)" ; \
                                 eval "$(pyenv init -)" ; \
                                 eval "$(pyenv virtualenv-init -)"
 
-[ -d "${HOME}/.tfenv/bin" ] &&  PATH="${HOME}/.tfenv/bin:${PATH}"
-
-[ -d "${HOME}/.poetry/bin" ] && PATH="${HOME}/.poetry/bin:${PATH}"
+[ -d "${HOME}/.local/poetry/bin" ] && PATH="${HOME}/.local/poetry/bin:${PATH}"
 
 # set main editor
 export EDITOR="nvim"
 export MYVIMRC=${HOME}/.config/nvim/init.vim
 
 # Nvidia cuda cli tool
+export PATH=/usr/local/cuda/bin:$PATH
+export CUDA_ROOT=/usr/local/cuda
 export PATH=/usr/local/cuda-11.3/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
 
 [ -f "${HOME}/.zshrc" ] && source "${HOME}/.zshrc"
--- install.sh
+++ install.sh
@@ -8,98 +8,98 @@ set -euo pipefail
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade
 
 ## Adding repos
-# Yarn repo
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-# Add nodejs repositories
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-
+mkdir -p ${HOME}/.local/{pyenv/bin,poetry/bin,go,bin,aws-cli/v2}
 # Install base packages
 sudo apt update && \
 sudo apt install -y --fix-broken git wget curl tmux autojump parallel \
-                    apt-transport-https ca-certificates gnupg fzf neovim \
-                    universal-ctags \
-                    timewarrior stow flatpak \
-                    software-properties-common build-essential \
-                    g++ gcc llvm make yarn nodejs \
+                    apt-transport-https ca-certificates \
+                    gnupg fzf stow flatpak software-properties-common \
+                    build-essential g++ gcc llvm make \
                     libglu1-mesa libfreeimage3 libxi-dev libx11-dev \
                     libxmu-dev freeglut3-dev libglu1-mesa-dev libfreeimage-dev
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
+    chmod u+x nvim.appimage && mv nvim.appimage ${HOME}/.local/bin/nvim
 
 [ ! -d "${ZSH_CUSTOM:=${HOME}/.oh-my-zsh/custom}/plugins/zsh-completions" ] && \
     git clone https://github.com/zsh-users/zsh-completions \
         ${ZSH_CUSTOM:=${HOME}/.oh-my-zsh/custom}/plugins/zsh-completions
+
 [ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k" ] && \
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
         ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
 # Install pyenv requirements
-[ ! -d "${HOME}/.pyenv" ] && sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-                                libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev \
-                                libffi-dev liblzma-dev python-openssl && curl https://pyenv.run | bash \
-                                || ${HOME}/.pyenv/bin/pyenv update
+[ ! -d "${HOME}/.local/pyenv" ] &&
+                                sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+                                    libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+                                    libffi-dev liblzma-dev python-openssl && \
+                                curl https://pyenv.run | bash &&\
+                                mv ${HOME}/.pyenv/* ${HOME}/.local/pyenv/ \
+                                || pyenv update
 
 # install poetry
-[ ! -d ${HOME}/.poetry ] && \
-    sudo apt install -y python3-dev python3-pip; \
-        python3 -m pip install --upgrade bpytop pip; \
-        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -; \
-    mkdir -p $ZSH_CUSTOM/plugins/poetry/ ; \
-    poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry ; \
-    poetry config virtualenvs.in-project true
+[ ! -d "${HOME}/.local/poetry" ] && \
+    rm -rf "${HOME}/.local/poetry/*" && \
+    sudo apt install -y python3-dev python3-pip && \
+    python3 -m pip install --upgrade pip && \
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py |  \
+        POETRY_HOME="${HOME}/.local/poetry" python3 python3 - && \
+    mkdir -p $ZSH_CUSTOM/plugins/poetry/ && poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry && \
+    poetry config virtualenvs.in-project true || poetry self update
 
 # Install go and go shell completion
-sudo rm -rf /usr/local/go \
-&& wget https://golang.org/dl/go1.17.6.linux-amd64.tar.gz  -O - | sudo tar -C /usr/local -xzf -
+wget https://golang.org/dl/go1.22.2.linux-amd64.tar.gz  -O - | sudo tar -C ${HOME}/.local/go -xzf -
 
-github_latest_dl hadolint/hadolint ".*x86_64" "Linux" /usr/local/bin/hadolint \
-    && sudo chmod +x /usr/local/bin/hadolint
+wget -O ${HOME}/.local/bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Darwin-x86_64 \
+    && sudo chmod +x ${HOME}/.local/bin/hadolint
 
 #install tfenv
-git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+git clone --depth=1 https://github.com/tfutils/tfenv.git ${HOME}/.local/tfenv && \
+    ln -s ${HOME}/.local/tfenv/bin ${HOME}/.local/bin
 
+# install azure CLI
 # Microsoft signing keys
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | \
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
-
-# install
 sudo apt autoclean autoremove \
     && sudo apt-get update \
     && sudo apt upgrade -y \
-    && sudo apt-get -y install azure-cli
-
-# Download Kubectx
-[ ! -f "${HOME}/.local/bin/kubectx" ] && \
-    curl https://api.github.com/repos/ahmetb/kubectx/releases/latest \
-        | grep -i "browser_download_url" | grep "/kubectx\"" | cut -d '"' -f 4 \
-        | wget -i - -O ${HOME}/.local/bin/kubectx \
-    && chmod +x ~/.local/bin/kubectx
-# Download Kubens
-[ ! -f "${HOME}/.local/bin/kubens" ] && \
-    curl https://api.github.com/repos/ahmetb/kubectx/releases/latest \
-        | grep -i "browser_download_url" | grep "/kubens\"" | cut -d '"' -f 4 \
-        | wget -i - -O ${HOME}/.local/bin/kubens \
-    && chmod +x ~/.local/bin/kubens
-
-# Kubens and kubectx zsh completion
-[ ! -d "${HOME}/.oh-my-zsh/completion" ] && \
-    mkdir -p ${HOME}/.oh-my-zsh/completions && \
-    chmod -R 755 ${HOME}/.oh-my-zsh/completions && \
-    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubectx.zsh \
-        -O ${HOME}/.oh-my-zsh/completions/_kubectx.zsh && \
-    wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubens.zsh \
-        -O ${HOME}/.oh-my-zsh/completions/_kubens.zsh && \
+    && sudo apt-get -y install azure-cli && az config set auto-upgrade.enable=yes
 
 # AWS cli version 2 Install
-[ ! -d "/usr/local/aws-cli/v2/" ] && \
+[ -d "${HOME}/.local/aws-cli/" ] && \
     wget -O ${HOME}/Downloads/awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip \
     && unzip ${HOME}/Downloads/awscliv2.zip -d ${HOME}/Downloads/ \
-    && sudo ${HOME}/Downloads/aws/install \
+    && sudo ${HOME}/Downloads/aws/install --bin-dir "${HOME}/.local/bin" --install-dir "${HOME}/.local/aws-cli/ --update" \
     && rm -rf ${HOME}/Downloads/awscliv2.zip ${HOME}/Downloads/aws
 
-stow --restow --target=${HOME} dotfiles
+# # Download Kubectx
+# [ ! -f "${HOME}/.local/bin/kubectx" ] && \
+#     curl https://api.github.com/repos/ahmetb/kubectx/releases/latest \
+#         | grep -i "browser_download_url" | grep "/kubectx\"" | cut -d '"' -f 4 \
+#         | wget -i - -O ${HOME}/.local/bin/kubectx \
+#     && chmod +x ~/.local/bin/kubectx
+# # Download Kubens
+# [ ! -f "${HOME}/.local/bin/kubens" ] && \
+#     curl https://api.github.com/repos/ahmetb/kubectx/releases/latest \
+#         | grep -i "browser_download_url" | grep "/kubens\"" | cut -d '"' -f 4 \
+#         | wget -i - -O ${HOME}/.local/bin/kubens \
+#     && chmod +x ~/.local/bin/kubens
+# # Kubens and kubectx zsh completion
+# [ ! -d "${HOME}/.oh-my-zsh/completion" ] && \
+#     mkdir -p ${HOME}/.oh-my-zsh/completions && \
+#     chmod -R 755 ${HOME}/.oh-my-zsh/completions && \
+#     wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubectx.zsh \
+#         -O ${HOME}/.oh-my-zsh/completions/_kubectx.zsh && \
+#     wget https://raw.githubusercontent.com/ahmetb/kubectx/master/completion/_kubens.zsh \
+#         -O ${HOME}/.oh-my-zsh/completions/_kubens.zsh
 
-echo "Done"
+# stow --restow --target=${HOME} dotfiles
+
+# echo "Done"
