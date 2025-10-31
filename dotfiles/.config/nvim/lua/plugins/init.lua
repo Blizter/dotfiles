c42d9ebb4cf70b488f9f1aac4020a994a92e41a2
Eric Hammel <ehammel@Erics-MacBook-Pro.local>
2025-10-31 00:29:24 -0400

    add nvim configs

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -1,48 +1,28 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require "configs.lspconfig"
-        end,
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "black", "isort" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                terraform = { "terraform-ls" },
-                markdown = { "markdown_oxide" },
-            },
-        },
-    },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "lua-language-server",
-                "stylua",
-                "prettier",
-                "pyright",
-                "black",
-                "isort",
-                "flake8",
-                "terraform-ls",
-                "markdown_oxide",
-            },
-        },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim",
-                "lua",
-                "vimdoc",
-                "python",
-                "terraform",
-                "markdown",
-            },
-        },
-    },
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
--- dotfiles/.zshenv
+++ dotfiles/.zshenv
@@ -12,6 +12,8 @@ export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
+export SECRETS_HOME="${HOME}/.local/secrets"
+
 export EDITOR="nvim"
 export KUBE_EDITOR="nvim"
 
